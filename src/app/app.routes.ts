8604ec5392b1c4ef6e5cc1217562ee382991d44f
sbarranco <sbarrancovico@gmail.com>
2025-05-04 22:24:24 +0200

    feat: update routing and configuration for event catalog and shopping cart components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  {
+    path: 'events',
+    loadComponent: () =>
+      import(
+        './features/events/pages/event-catalog/event-catalog.component'
+      ).then((m) => m.EventCatalogComponent),
+  },
+  {
+    path: 'events/:id',
+    loadComponent: () =>
+      import(
+        './features/events/pages/event-detail/event-detail.component'
+      ).then((m) => m.EventDetailComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/cart/pages/shopping-cart.component').then(
+        (m) => m.ShoppingCartComponent
+      ),
+  },
+  { path: '**', redirectTo: 'events' },
+];
