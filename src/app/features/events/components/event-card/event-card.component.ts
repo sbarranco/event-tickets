ad89604403d765abba71a1899a53dd64e8bf7ffd
sbarranco <sbarrancovico@gmail.com>
2025-05-06 22:01:01 +0200

    feat: add EventCard component  files

---
+++ src/app/features/events/components/event-card/event-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+import { Event } from '../../models/event.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss'],
+  imports: [CommonModule, ButtonComponent],
+})
+export class EventCardComponent {
+  event = input.required<Event>();
+  clickCard = output<string>();
+  isMediumOrLargeScreen: any;
+}
